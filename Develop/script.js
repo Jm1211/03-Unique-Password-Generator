79b43ca828a7013794279ee481f7cc4643301e9f
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-10-01 17:25:01 -0600

    add promps for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -2,24 +2,36 @@
 var generateBtn = document.querySelector("#generate");
 
 var specialChar = "'!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-var numbers = (0,1,2,3,4,5,6,7,8,9)
-var alpha = ('a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z')
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
+var upperCase = []
 
-var passwordLength
-var confirmUppercase
-var confirmLowercase
-var confirmSpecialChar
-var confirmNumber
-
-var toUpper = function (x) {
-  return x.toUpperCase();
-}
+//var toUpper = function (x) {
+ // return x.toUpperCase();
+//}
 
 
 
 function generatePassword(){
-
-    window.prompt("How many characters would you like your password to be? ")
+// prompt user for password length 8-128 characters
+    var userInput = window.prompt("How many characters would you like your password to be? (8-128 characters) ")
+    var passwordLength =parseInt(userInput)
+//alerts user password needs to be a number and confirms length     
+    if(isNaN(passwordLength)){
+      alert ("Please enter digit");
+      return false
+    }
+    if(passwordLength < 8 || passwordLength >128){
+      alert("Password must contain 8-128 characters. please try again")
+      return false
+    }
+   
+    confirmLowercase = window.confirm("Would you like to include lowercase?")
+    confirmUppercase = window.confirm("Would you like to include uppercase?")
+    confirmNumber = window.confirm("Would you like to include numbers?")
+    confirmSpecialchar = window.confirm("Would you like to include special characters?")
+  
+    return "generated password"
 }
 
 
