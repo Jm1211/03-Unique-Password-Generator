8d05861e3b8cc404b778ab7ea28d145d2a20f621
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-10-02 18:08:09 -0600

    fix upper and lower case prompts, add alert for not choosing parameters

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var specialChar = ["!","@","#","$","%","^","&","*","(",")","=","+","~","<",">","[","]","{","}","?","/","-","_",".",":",";",]
+var specialChar = ["!","@","#","$","%","^","&","*","(",")","=","+","~","<",">","[","]","{","}","?","/","-","_",".",":",";"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var options = ""
+var options = "";
 
 
 function generatePassword(){
@@ -23,10 +23,11 @@ function generatePassword(){
       return false
     }
   var options = "";
-    if(confirmLowercase = window.confirm("Would you like to include lowercase?") == true ){
+
+    if(confirmlowerCase = window.confirm("Would you like to include lowercase?") == true ){
        options = options.concat(lowerCase);
   }
-    if(confirmUppercase = window.confirm("Would you like to include uppercase?") == true ){
+    if(confirmupperCase = window.confirm("Would you like to include uppercase?") == true ){
        options = options.concat(upperCase);
   }
     if(confirmNumber = window.confirm("Would you like to include numbers?") == true ){
@@ -35,11 +36,16 @@ function generatePassword(){
     if(confirmSpecialchar = window.confirm("Would you like to include special characters?") == true ){
        options= options.concat(specialChar);
   }
+    if (!confirmSpecialchar & !confirmNumber & !confirmupperCase & !confirmlowerCase) {
+      options = alert("You must choose a parameter");
+
+  }
 
   var  finalPass = "";
 
   for(var i = 0; i < passwordLength; i++){
-    finalPass= Math.floor(Math.random()*options.length);
+    var randomIndex = Math.floor(Math.random()*options.length);
+    finalPass = finalPass + options[randomIndex];
   }
   
   return finalPass;
